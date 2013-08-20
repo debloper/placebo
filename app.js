963446cbb2695b2857b250d2f6ad28d839c7de52
Soumya Deb <debloper@gmail.com>
2013-08-21 05:20:51 +0530

    Added a opt-in node.js-based static file server

---
+++ app.js
@@ -0,0 +1,8 @@
+// Requiring basic modules
+var http = require("http")
+,	ecsy = require("ecstatic");
+
+// Creating the file server
+http.createServer(
+	ecsy({ root: __dirname + "/" })
+).listen(8080);
