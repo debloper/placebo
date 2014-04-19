e1357492feae21793611d6bc9fb91fabf0779741
Soumya Deb <debloper@gmail.com>
2014-04-20 02:53:16 +0530

    HTML cleanups, indentation & spacing fixes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Requiring basic modules
 var http = require("http")
-,	ecsy = require("ecstatic");
+,	  ecsy = require("ecstatic");
 
 // Creating the file server
 http.createServer(
-	ecsy({ root: __dirname + "/" })
+  ecsy({ root: __dirname + "/" })
 ).listen(8080);
